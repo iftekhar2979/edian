c4bde6aebdc49ac586df52e12b8d99e785715f6f
iftekhar2979 <salminifti79@gmail.com>
2025-07-13 23:44:02 +0600

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['edinamag.com','essentialsaltspa.com', 'images.unsplash.com', 's.alicdn.com'], // Add actual domains used
+  },
 };
 
 export default nextConfig;
